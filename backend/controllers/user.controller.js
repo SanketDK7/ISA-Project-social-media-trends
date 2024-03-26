5216f0cea7b55cf3845c6cb117fc8b89e24db23a
Shivanii30 <kshirsagarshivani1438@gmail.com>
2024-03-27 02:01:20 +0530

    CRUD User APi logic

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -20,8 +20,39 @@ const singleUser = async(req,res) =>{
     }
 };
 
+const createUser = async(req,res) => {
+    try {
+        const user = await User.create(req.body);
+        res.status(200).json(user);
+        
+    } catch (error) {
+        res.status(500).json({message: error.message});
+        
+    }
+
+};
+
+const updateUser =  async(req, res) =>{
+    try {
+        const {id} = req.params;
+
+        const user = await User.findByIdAndUpdate(id, req.body);
+
+        if(!user){
+            return res.status(404).json("No user with this id");
+        }
+        const updatedUser = await User.findById(id);
+        res.status(200).json(updatedUser);
+
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+};
+
 
 module.exports={
     getUsers,
-    singleUser
+    singleUser,
+    createUser,
+    updateUser
 }
