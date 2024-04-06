b74812de8608ee10d5122a7314348d1106ca08df
Shivanii30 <kshivaniofficial@gmail.com>
2024-04-06 13:25:05 +0530

    User Authenticate

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -2,7 +2,7 @@ const express = require('express');
 const User = require("../models/user.model.js");
 const router = express.Router();
 const {fetchPopularVideos} = require('../controllers/youtube.controller.js')
-const {getUsers,singleUser,createUser,updateUser,delUser} = require('../controllers/user.controller.js')
+const {getUsers,singleUser,createUser,updateUser,delUser,authenticateUser} = require('../controllers/user.controller.js')
 
 router.get('/', getUsers);
 
@@ -13,6 +13,8 @@ router.post("/", createUser);
 router.put('/:id', updateUser);
 
 router.delete('/:id', delUser);
+
+router.post('/login', authenticateUser);
 
 router.get('/ytTrends', fetchPopularVideos);
 
